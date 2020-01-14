bf3c45dd0c37d805eca33d1140633f6d5199d557
Darrel O'Pry <darrel.opry@spry-group.com>
2020-01-14 01:30:56 -0500

    chore: style demo.

--- src/App.svelte
+++ src/App.svelte
@@ -11,28 +11,34 @@ import {
 } from './components/components.module.js';
 </script>
 
-<style></style>
-
+<div class="container">
 <Auth0Context
   domain="dev-hvw40i79.auth0.com"
   client_id="aOijZt2ug6Ovgzp0HXdF23B6zxwA6PaP"
   callback_url="process.env.AUTH0_DEFAULT_CALLBACK_URL"
   logout_url="process.env.AUTH0_DEFAULT_CALLBACK_URL"
 >
-  <button on:click|preventDefault='{() => login() }'>Login</button>
-  <button on:click|preventDefault='{() => logout() }'>Logout</button><br />
-  <pre>isLoading: {$isLoading}</pre>
-  <pre>isAuthenticated: {$isAuthenticated}</pre>
-  <pre>authToken: {$authToken}</pre>
-  <pre>userInfo: {JSON.stringify($userInfo, null, 2)}</pre>
-  <pre>authError: {$authError}</pre>
+
+  <button class="btn" on:click|preventDefault='{() => login() }'>Login</button>
+  <button class="btn" on:click|preventDefault='{() => logout() }'>Logout</button>
+  <table>
+    <thead>
+      <tr><th>store</th><th>value</th></tr>
+    </thead>
+    <tbody>
+      <tr><td>isLoading</td><td>{$isLoading}</td></tr>
+      <tr><td>isAuthenticated</td><td>{$isAuthenticated}</td></tr>
+      <tr><td>authToken</td><td>{$authToken}</td></tr>
+      <tr><td>userInfo</td><td><pre>{JSON.stringify($userInfo, null, 2)}<pre></td></tr>
+      <tr><td>authError</td><td>{$authError}</td></tr>
+    </tbody>
+  </table>
 </Auth0Context>
 
-<p>
-If social login is not persisting across page reloads, you are most likely using Universal Login classic with Auth0 Dev
-keys for the social provider. There are two ways to resolve the issue:
-</p>
+<p>If social login is not persisting across page reloads, you are most likely using Universal Login classic with Auth0
+Dev keys for the social provider. There are two ways to resolve the issue:</p>
 <ol>
-  <li><a href="https://auth0.com/docs/universal-login/new">Switch to the New Universal Login Experience]()</a></li>
+  <li><a href="https://auth0.com/docs/universal-login/new">Switch to the New Universal Login Experience</a></li>
   <li><a href="https://auth0.com/docs/connections/social/google">Use your own keys for the Social connection</a></li>
 </ol>
+</div>
